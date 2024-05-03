32eafae7b55140d4116028ef913eb2a8fb9fdb11
j1nxie <rylieeeeexd@gmail.com>
2024-05-03 17:38:57 +0700

    feat: parsing a String for a chart

--- src/lib.rs
+++ src/lib.rs
@@ -88,3 +88,109 @@ pub struct ChuniChart {
     pub sfl: Vec<Sfl>,
     pub notes: Vec<NoteType>,
 }
+
+impl ChuniChart {
+    pub fn parse(input: String) -> Result<ChuniChart, ParseError> {
+        let mut chart = ChuniChart::default();
+        let mut current_section: ParserContext;
+
+        for line in input.lines() {
+            let line = line.trim();
+            if !line.trim().is_empty() {
+                current_section = ParserContext::get_section(line);
+                match current_section {
+                    ParserContext::Version => {
+                        let version = line.split('\t').nth(1).unwrap();
+                        chart.version = version.to_string();
+                    }
+
+                    ParserContext::Music => {
+                        let music = line.split('\t').nth(1).unwrap();
+                        chart.music = music.parse::<usize>().unwrap();
+                    }
+
+                    ParserContext::SequenceId => {
+                        let sequence_id = line.split('\t').nth(1).unwrap();
+                        chart.sequence_id = sequence_id.parse::<usize>().unwrap();
+                    }
+
+                    ParserContext::Difficult => {
+                        let difficult = line.split('\t').nth(1).unwrap();
+                        chart.difficult = difficult.parse::<usize>().unwrap();
+                    }
+
+                    ParserContext::Level => {
+                        let level = line.split('\t').nth(1).unwrap();
+                        chart.level = level.parse::<f64>().unwrap();
+                    }
+
+                    ParserContext::Creator => {
+                        let creator = line.split('\t').nth(1).unwrap();
+                        chart.creator = creator.to_string();
+                    }
+
+                    ParserContext::BpmDef => {
+                        let bpm_def = BpmDef::from_str(line).unwrap();
+                        chart.bpm_def = bpm_def;
+                    }
+
+                    ParserContext::MetDef => {
+                        let met_def = MetDef::from_str(line).unwrap();
+                        chart.met_def = met_def;
+                    }
+
+                    ParserContext::Resolution => {
+                        let resolution = line.split('\t').nth(1).unwrap();
+                        chart.resolution = resolution.parse::<usize>().unwrap();
+                    }
+
+                    ParserContext::ClkDef => {
+                        let clk_def = line.split('\t').nth(1).unwrap();
+                        chart.clk_def = clk_def.parse::<usize>().unwrap();
+                    }
+
+                    ParserContext::ProgJudgeBpm => {
+                        let progjudge_bpm = line.split('\t').nth(1).unwrap().trim();
+                        chart.progjudge_bpm = progjudge_bpm.parse::<f64>().unwrap();
+                    }
+
+                    ParserContext::ProgJudgeAer => {
+                        let progjudge_aer = line.split('\t').nth(1).unwrap().trim();
+                        chart.progjudge_aer = progjudge_aer.parse::<f64>().unwrap();
+                    }
+
+                    ParserContext::Tutorial => {
+                        let tutorial = line.split('\t').nth(1).unwrap().parse::<usize>().unwrap();
+                        chart.tutorial = tutorial != 0;
+                    }
+
+                    ParserContext::Bpm => {
+                        let bpm = Bpm::from_str(line).unwrap();
+                        chart.bpm.push(bpm);
+                    }
+
+                    ParserContext::Met => {
+                        let met = Met::from_str(line).unwrap();
+                        chart.met.push(met);
+                    }
+
+                    ParserContext::Sfl => {
+                        let sfl = Sfl::from_str(line).unwrap();
+                        chart.sfl.push(sfl);
+                    }
+
+                    ParserContext::Note => {
+                        let note = NoteType::from_str(line).unwrap();
+                        chart.notes.push(note);
+                    }
+
+                    ParserContext::None => continue,
+
+                    _ => unreachable!(),
+                }
+            }
+        }
+
+        Ok(chart)
+    }
+}
